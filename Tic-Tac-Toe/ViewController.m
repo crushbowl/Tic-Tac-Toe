33d26017d05cd1b346488c7ca59cf5462dd5ab6a
JanL <janelle.h.writes@gmail.com>
2016-05-28 20:04:27 -0500

    Add buttons and X, O functionality, player turn, and blue/red color of player

--- Tic-Tac-Toe/ViewController.m
+++ Tic-Tac-Toe/ViewController.m
@@ -9,19 +9,72 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *buttonOne;
+@property (weak, nonatomic) IBOutlet UIButton *buttonTwo;
+@property (weak, nonatomic) IBOutlet UIButton *buttonThree;
+@property (weak, nonatomic) IBOutlet UIButton *buttonFour;
+@property (weak, nonatomic) IBOutlet UIButton *buttonFive;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSix;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSeven;
+@property (weak, nonatomic) IBOutlet UIButton *buttonEight;
+@property (weak, nonatomic) IBOutlet UIButton *buttonNine;
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *buttons;
+
+
+@property BOOL isX;
+@property BOOL shouldSave;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+-(void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.isX = true;
+    [self showCurrentPlayer];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+// When a user clicks on a button, the sender (indiv button) fires and records the current Player Label
+// in the button Title text
+
+-(IBAction)onButtonTapped:(UIButton *)sender {
+    int x = 1;
+    for (UIButton *button in self.buttons) {
+        if (sender == button) {
+            [button setTitle:self.whichPlayerLabel.text forState:UIControlStateNormal];
+            [button setTitleColor: self.whichPlayerLabel.textColor forState:UIControlStateNormal];
+            NSLog(@"Button #: %i - Name: %@",x,button.titleLabel.text); }
+        x++;
+    }
+    [self showCurrentPlayer];
 }
+
+
+-(void)showCurrentPlayer{
+    self.isX = !(self.isX);
+
+    if (self.isX) {
+        self.whichPlayerLabel.text = @"X";
+    } else {
+        self.whichPlayerLabel.text = @"O";
+    }
+
+    // label color
+}
+
+
+-(void)saveCurrentPosition {
+    
+    if (self.shouldSave) {
+        self.whichPlayerLabel.text = @"X";
+    } else {
+        self.whichPlayerLabel.text = @"0";
+    }
+
+}
+
+
 
 @end
